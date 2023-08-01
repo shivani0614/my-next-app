0b0b31f2a7a399bec6fc94b87b44b836b34c6fde
shivani0614 <shivanialeti14@gmail.com>
2023-07-31 21:00:20 -0500

    404 PAGE BENIFITS

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function NotFound(){
+    return(
+        <Layout>
+            <h1>not found</h1>
+        </Layout>
+        
+    )
+}
